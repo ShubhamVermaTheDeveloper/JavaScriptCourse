8b5cb035163f8a5b173c7e33a3ff072651b123ca
shubham <shubham25122001@gmail.com>
2023-05-31 12:04:17 +0530

    04 Primitive object

---
+++ 04 Primitive object/index.js
@@ -0,0 +1,24 @@
+// nn bb ss u premitives datatypes
+
+let a = null;
+let b = 345;
+let c = true;
+let d =  BigInt("567") + BigInt("3")
+let e = "SHUBHAM"  
+let f = Symbol("I am a nice symbol")
+let g = undefined
+
+
+console.log(a, b, c, d, e, f, g)
+console.log(typeof d)
+
+//Object in Js -- Non primitive data type
+const item ={
+  "shubham": true,
+  "Shubh": false,
+  "Lovish": 67,
+  "Rohan": undefined
+}
+
+console.log(item[shubham])
+  
