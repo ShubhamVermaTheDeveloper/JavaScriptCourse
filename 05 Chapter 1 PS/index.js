ce35965f66fc516a2662928dd324030e25e1e5f9
shubham <shubham25122001@gmail.com>
2023-05-31 12:05:08 +0530

    05 Chapter 1 PS

---
+++ 05 Chapter 1 PS/index.js
@@ -0,0 +1,30 @@
+// Chapter 1  Q1
+let a = "Shubham"
+let b = 6;
+console.log(a + b)
+
+// Chapter 1  Q2
+console.log(typeof (a+b))
+
+// Chapter 1  Q3
+const a1 = {
+  name: "shubham",
+  section: 1,
+  isPrincipal: false
+}
+
+//Q4
+// a1 = 45
+
+a1['friend'] = "shubham"
+a1['name'] = "Lavis"
+console.log(a1)
+
+
+const dict = {
+  appreciate: "recognize the full worth of.",
+  apartaxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work"
+}
+
+console.log(dict.yakka)
