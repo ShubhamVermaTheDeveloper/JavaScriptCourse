4423fcf1d561d9d7cc5556d4d2bf534465b1a3f6
shubham <shubham25122001@gmail.com>
2023-05-31 11:56:44 +0530

    02 Variables

---
+++ 02 Variables/idex.js
@@ -0,0 +1,8 @@
+console.log("this repl contains code for the Ultimae JavaScript couse video no 2")
+var a = 67
+console.log(a)
+a = "shubham"
+console.log(a)
+
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7 // this is also throw error
