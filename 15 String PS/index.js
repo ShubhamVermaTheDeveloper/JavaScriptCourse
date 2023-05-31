5db0475a7cf8c5174c29ea4d93998c68ebd8e890
shubham <shubham25122001@gmail.com>
2023-05-31 12:22:39 +0530

    15 String PS

---
+++ 15 String PS/index.js
@@ -0,0 +1,23 @@
+// Problem no. 1
+console.log("har\"\"".length)
+
+// Problem no. 2
+const sentence = 'this quick brown fox jumps over the lazy dog.';
+const word = 'fox';
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`); 
+
+// Problem no. 3
+let str = "Hello Ny Name is Shubham Verma";
+console.log(str.toLowerCase());
+
+// Problem no. 4
+let str2 = "Please give Rs 1000"
+let amount = str2.slice("Please give Rs".length)
+console.log(amount)
+
+
+// Problem 5
+let friend = "Deepika"
+friend = friend.replace("p", "R")
+console.log(friend)
+
