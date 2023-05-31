ca5dcf90156c782f4ed8901df3c9fb6497c2bdd7
shubham <shubham25122001@gmail.com>
2023-05-31 12:16:54 +0530

    11 Functions

---
+++ 11 Functions/index.js
@@ -0,0 +1,28 @@
+
+const hello =()=>{
+    console.log("Hey how are you. I am toh fine yar")
+    return "hi"
+  }
+  
+  function onePlusAvg(x, y){
+    return Math.round(1 + (x + y)/2)
+  }
+  
+  const sum = (a, b)=> {
+    return (a + b)
+  }
+  
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  
+  
+  
+  console.log("Average of a and b is : "+ onePlusAvg(a,b))
+  console.log("Average of c and c is : "+ onePlusAvg(b,c))
+  console.log("Average of a and c is : "+ onePlusAvg(a,c))
+  
+  console.log("sum is : "+sum(a,b))
+  console.log(hello())
+  // console.log(v)
+  
