0afb0c518dba2c3ccdd41aa0e9d5c635de925ff2
shubham <shubham25122001@gmail.com>
2023-05-31 12:40:57 +0530

    30 Snack Water Gun

---
+++ 30 Snack Water Gun/script.js
@@ -0,0 +1,1 @@
+let option = prompt("Choose your option : Snack - Water - Gun \n                                       |           |            |\n                                       s   -    w    -   g")
