c17c8b5ae5c80c218e492a0c485d3c31ab52f600
shubham <shubham25122001@gmail.com>
2023-05-31 12:05:54 +0530

    06 Operators

---
+++ 06 Operators/index.js
@@ -0,0 +1,58 @@
+// 77;
+// "shubham verma"
+// false
+
+console.log("Operator is js")
+let a = 45;
+let b = 4; 
+// Arithmetic operators
+console.log("<-------- Arithmetic operators------------->")
+console.log("a + b = ", a+b)  //49
+console.log("a - b = ", a-b)  //41
+console.log("a / b = ", a/b)  // 11.25
+console.log("a * b = ", a*b)  // 180
+console.log("a ** b = ", a**b)  //41006525
+console.log("a % b = ", a%b)  //1
+console.log("++a = ", ++a)    //46
+console.log("a++ = ", a++)    //46
+console.log("a-- = ", a--)    //47
+console.log("--a = ", --a)    //45
+
+// Assignment Operators
+console.log("<-------- Assignment operators------------->")
+let assignment = 1
+assignment += 5           // assignment = 6
+console.log(assignment)
+
+
+// Comparison Operators
+console.log("<-------- Comparison operators------------->")
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is ", comp1 == comp2)   //false
+console.log("comp1 != comp2 is ", comp1 != comp2)   //true
+console.log("comp1 === comp2 is ", comp1 === comp2)  //false
+console.log("comp1 !== comp2 is ", comp1 !== comp2)   //true
+console.log("comp1 > comp2 is ", comp1 > comp2)        //false
+console.log("comp1 < comp2 is ", comp1 < comp2)         //true
+
+
+
+// Logical operators
+let x = 5;
+let y = 6;
+console.log("<-------- Logical operators------------->")
+console.log(x<y && x==5)   //true
+console.log(x>y && x==5)   //false 
+console.log(x>y || x==5)   //true
+console.log(x>y || x==5)   //true 
+console.log()   //true 
+
+
+
+// comment single line comment 
+
+/*--------------this is multiline comment----------------
+This is a course 
+for learning the javascript.
+*/
