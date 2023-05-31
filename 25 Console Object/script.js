106d67f0ca14f5e4ea98c438f44d2fddea0520d3
shubham <shubham25122001@gmail.com>
2023-05-31 12:34:38 +0530

    25 Console Object

---
+++ 25 Console Object/script.js
@@ -0,0 +1,16 @@
+let obj = {
+    shubham: 10,
+    ashish: 54,
+    ajay: "Hello"
+  }
+  
+  
+  console.log(console)
+  console.log("console")
+  console.info("info")
+  console.warn("warn")
+  console.error("error")
+  console.assert("err" != false)
+  console.assert("err" == false)
+  console.table(obj)
+  
