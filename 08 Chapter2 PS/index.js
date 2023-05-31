02c653c00ed1f42bb7e9cbd1735702a76ca73b21
shubham <shubham25122001@gmail.com>
2023-05-31 12:07:19 +0530

    08 Chapter2 PS

---
+++ 08 Chapter2 PS/index.js
@@ -0,0 +1,28 @@
+// let age = prompt("What is the your age?")
+// if(age>10 && age<20){
+//   console.log("Your afe lies between 10 and 20")
+// }
+// else{
+//   console.log("Your age doesnt lies between 10 and 20")
+// }
+
+
+// Q3 : the number is divisible by two and three
+
+// let num = prompt("Enter the number")
+// num = Number.parseInt(num)
+// if(num%2==0 && num%3==0){
+//   console.log("the number is divisible by 2 and 3")
+// }
+// else{
+//   console.log("the number is not divisible by 2 and 3")
+// }
+
+
+
+// Ternary operator
+
+let age = 20
+let a = age>18?"You can drive":"You can not drive"
+console.log(a)
+
