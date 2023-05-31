c400918fcd82b14d402ef8534e6e96bdfa0f8b19
shubham <shubham25122001@gmail.com>
2023-05-31 12:10:32 +0530

    10 While loop

---
+++ 10 While loop/index.js
@@ -0,0 +1,19 @@
+
+// while loo
+let n = prompt("Enter any number : ")
+n = Number.parseInt(n)
+/*
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++
+}
+
+*/
+
+// do while loo
+let j = 10
+do{
+  console.log(j)
+  j++
+}while(j < n)
