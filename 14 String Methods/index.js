a31acbc14047fe67b24ec55b2a5db74655b303ba
shubham <shubham25122001@gmail.com>
2023-05-31 12:22:03 +0530

    14 String Methods

---
+++ 14 String Methods/index.js
@@ -0,0 +1,23 @@
+let name = "Shubham"
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4))
+// console.log(name.slice(2))
+// console.log(name.replace("ub", "per"))
+
+// let friend = "Naman"
+// console.log(name.concat(" is a friend of ", friend), "Ok")
+// let friend2 = "     Meena             "
+// console.log(friend2.trim())
+
+let fr = "Shivika"
+// console.log(fr[0])
+// console.log(fr[1])
+// console.log(fr[2])
+// console.log(fr[3])
+
+for(let i=0; i<fr.length; i++){
+  console.log(fr[i])
+}
