f9f7d5997b84ec4f2eb7d2f99cedb2f960c8b108
shubham <shubham25122001@gmail.com>
2023-05-31 12:03:13 +0530

    03 var let const

---
+++ 03 var let const/index.js
@@ -0,0 +1,16 @@
+console.log("JavaScript tutorial 3: var, let, const")
+// var a = 45;
+// var a = "p";
+let b = "Shubham";
+const author = "shubham verma"
+let c = null;
+let d = undefined 
+
+{
+  let b = 'this'
+  console.log(b)
+}
+  console.log(b)
+
+
+console.log(author)
